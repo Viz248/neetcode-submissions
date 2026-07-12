ca0548e66fec8155be41b36a475a40d941a42779
Jordan <vizbiz248@gmail.com>
2026-07-12 20:41:45 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows=len(board)
+        cols=len(board[0])
+        print(rows, cols)
+
+        for row in board:
+            nums=[x for x in row if x!='.']
+            if len(nums)!=len(set(nums)):
+                print('bing')
+                return False
+
+        i=0
+        while i<cols:
+            nums=[]
+            for row in board:
+                if row[i]!='.':
+                    nums.append(row[i])
+            if len(nums)!=len(set(nums)):
+                print("WOW")
+                return False
+            i+=1
+
+        for box_col in range(0,9,3):
+            for box_row in range(0,9,3):
+                nums=[]
+                for j in range(box_col, box_col+3):
+                    for i in range(box_row, box_row+3):
+                        if board[i][j]!='.':
+                            nums.append(board[i][j])
+                if len(nums)!=len(set(nums)):
+                                return False
+        return True
