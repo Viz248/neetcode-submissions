bdb5532fed0dfcd12becc80f2c25c3bc78b243e5
Jordan <vizbiz248@gmail.com>
2026-07-28 22:47:06 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq1=Counter(s1)
+        lens1, lens2 = len(s1), len(s2)
+
+        if lens1>lens2:
+            return False
+
+        l,r=0,lens1-1
+        l2=list(s2[:lens1])
+        freq2=Counter(l2)
+
+        if freq1==freq2:
+                return True
+        while r+1<len(s2):
+            r+=1
+            freq2[s2[r]]+=1
+            freq2[s2[l]]-=1 
+
+            if not freq2[s2[l]]:
+                del freq2[s2[l]]
+            l+=1
+            
+            if freq1==freq2:
+                return True
+        return False
